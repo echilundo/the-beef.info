b4f409f2f2c4a3d4f43b4ec69605a0c42b4fc18f
Edson C. <enerqy@gmail.com>
2025-03-31 12:48:23 +0200

    25c
    * More optimisations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -41,5 +41,41 @@ export default defineConfig({
         }
       },
     },
-  })
+  }),
+  build: {
+    inlineStylesheets: 'auto',
+    assets: 'assets',
+    splitting: true,
+  },
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+          pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.trace'],
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'critical': [
+              './src/components/Head.astro',
+              './src/layouts/Layout.astro'
+            ],
+            'router': ['@astrojs/client-router'],
+          }
+        }
+      }
+    },
+    optimizeDeps: {
+      include: ['@astrojs/client-router'],
+      exclude: ['@astrojs/client-router/virtual'],
+    },
+    ssr: {
+      noExternal: ['@astrojs/client-router']
+    }
+  }
 });
